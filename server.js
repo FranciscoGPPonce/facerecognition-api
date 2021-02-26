e2804525da289d03847c6b186b934f4c9bab20a0
Francisco G P Ponce <64283757+FranciscoGPPonce@users.noreply.github.com>
2021-02-26 17:59:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'sjdm7536',
-    database: 'facerecognition'
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+      }
     }
 });
 
