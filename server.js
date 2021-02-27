692025231c86e75b8f010786fa626983e55720b8
Francisco G P Ponce <64283757+FranciscoGPPonce@users.noreply.github.com>
2021-02-26 21:12:26 -0300

    Fix db

--- server.js
+++ server.js
@@ -11,10 +11,9 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
-        user : 'aneagoie',
-        password : '',
-        database : 'smart-brain'
+        connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
       }
     });
 
@@ -38,4 +37,3 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on ${process.env.PORT}`);
 })
-
